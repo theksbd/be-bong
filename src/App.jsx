c195e0ac485a7bf1940864b0338090d196594048
Do Thien Hoang <theksbd@gmail.com>
2025-01-17 20:59:48 +0700

    add image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import pic1 from '../xi_trum/Messenger_creation_86E2A5C0-9CEE-4DB8-BE2F-DAEFE97B950D.jpeg';
 
 function App() {
   return (
     <>
       <section>Hình mà anh thích nhất</section>
+      <img src={pic1} width={300} alt='anh1' />
     </>
   );
 }
